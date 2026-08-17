62afe45d02ee0337bf8f6639c4b54ba0914df76f
thiraput <mju6804101329@mju.ac.th>
2026-08-17 11:34:02 +0700

    first

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage(){
+    return(
+        <div>
+            <h1>about</h1>
+            <p>student</p>
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/components/Navber"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,6 +24,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        <header className="siteHeader">
+          <Navbar />
+        </header>
+
+        {children}
+      </body>
     </html>
   );
 }
