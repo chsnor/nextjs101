96f4a9ea3365290df206463d22bf0af13d9eb4b2
thiraput <mju6804101329@mju.ac.th>
2026-08-31 10:16:43 +0700

    seperate commponent

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,66 +1,14 @@
-type Course = {
-  id: number;
-  code: string;
-  title: string;
-  credits: number;
-  isOpen: boolean;
-};
-
-
-
-const courses: Course[] = [
-  {
-    id: 1,
-    code: "10301231",
-    title: "Web Technology",
-    credits: 3,
-    isOpen: true,
-  },
-  {
-    id: 2,
-    code: "10301232",
-    title: "Database Systems",
-    credits: 3,
-    isOpen: false,
-  },
-  {
-    id: 3,
-    code: "10301233",
-    title: "Data Structures and Algorithms",
-    credits: 3,
-    isOpen: true,
-  },
-  {
-    id: 4,
-    code: "10301234",
-    title: "Software Engineering",
-    credits: 3,
-    isOpen: true,
-  },
-  {
-    id: 5,
-    code: "10301235",
-    title: "Computer Networks",
-    credits: 3,
-    isOpen: false,
-  },
-];
+import CourseCard from "@/components/CourseCard";
+import {courses} from "@/data/coursesdata"
 
 export default function CoursesPage() {
   return (
-    <main className="page">
-      <h1>รายวิชาทั้งหมด</h1>
-
-      <section className="courseGrid">
-        {courses.map((course) => (
-          <article key={course.id} className="courseCard">
-            <h2>{course.title}</h2>
-            <p>รหัสวิชา: {course.code}</p>
-            <p>{course.credits} หน่วยกิต</p>
-            <p>{course.isOpen ? "เปิดลงทะเบียน" : "ปิดลงทะเบียน"}</p>
-          </article>
-        ))}
-      </section>
-    </main>
+      <>
+        <section className="courseGrid">
+          {courses.map((course) => (
+            <CourseCard key={course.id} course={course} />
+          ))}
+        </section>
+      </>
   );
 }
